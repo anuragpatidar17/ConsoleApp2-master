7caadcc39e618cb1d531297e83ed6fbd1023076f
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-02-14 23:46:05 +0530

    first

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+using NPOI.Util;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+
+namespace ConsoleApp2
+{
+
+    public class Class1
+    {
+        [Test]
+        public void SearchForWord()
+        {
+            var driver = new ChromeDriver();
+
+            {
+           
+            //FileStream file = new FileStream(@"/Users/anuragpatidar/Desktop/ConsoleApp2-master/Book1.xlsx", FileMode.Open, FileAccess.Read);
+                FileStream file = new FileStream(@"D:\a\1\s\Book1.xlsx", FileMode.Open, FileAccess.Read);
+                XSSFWorkbook workbook = new XSSFWorkbook(file);
+                ISheet sheet = workbook.GetSheet("Sheet1");
+
+                var value = string.Format(sheet.GetRow(0).GetCell(0).StringCellValue);
+
+
+
+                driver.Navigate().GoToUrl("http://www.google.com/");
+
+
+                IWebElement query = driver.FindElement(By.Name("q"));
+
+
+                Console.WriteLine(driver.Title + value);
+
+                query.SendKeys(value);
+
+
+                query.Submit();
+
+
+
+                driver.Quit();
+            }
+
+        }
+    }
+}
