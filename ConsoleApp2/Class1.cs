2a9a9a288fc6a31636e6b981746a23c83e99a4f4
anuragpatidar17 <anurag.patidar@yahoo.in>
2021-02-19 15:44:35 +0530

    download

--- ConsoleApp2/Class1.cs
+++ ConsoleApp2/Class1.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using NPOI.Util;
 using NPOI.XSSF.UserModel;
 using NPOI.SS.UserModel;
+using System.Threading;
 
 namespace ConsoleApp2
 {
@@ -19,15 +20,16 @@ namespace ConsoleApp2
         public void SearchForWord()
         {
             var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
 
             {
            
             //FileStream file = new FileStream(@"/Users/anuragpatidar/Desktop/ConsoleApp2-master/Book1.xlsx", FileMode.Open, FileAccess.Read);
-                FileStream file = new FileStream(@"D:\a\1\s\Book1.xlsx", FileMode.Open, FileAccess.Read);
-                XSSFWorkbook workbook = new XSSFWorkbook(file);
-                ISheet sheet = workbook.GetSheet("Sheet1");
+                //FileStream file = new FileStream(@"D:\a\1\s\Book1.xlsx", FileMode.Open, FileAccess.Read);
+                //XSSFWorkbook workbook = new XSSFWorkbook(file);
+                //ISheet sheet = workbook.GetSheet("Sheet1");
 
-                var value = string.Format(sheet.GetRow(0).GetCell(0).StringCellValue);
+                //var value = string.Format(sheet.GetRow(0).GetCell(0).StringCellValue);
 
 
 
@@ -37,12 +39,26 @@ namespace ConsoleApp2
                 IWebElement query = driver.FindElement(By.Name("q"));
 
 
-                Console.WriteLine(driver.Title + value);
+                string excel_demo = "#rso > div:nth-child(1) > div:nth-child(1) > div > div.yuRUbf > a > div > cite";
 
-                query.SendKeys(value);
-
+                query.SendKeys("download  sample excel");
 
                 query.Submit();
+
+                Thread.Sleep(2000);
+
+                driver.FindElement(By.CssSelector(excel_demo)).Click();
+
+                
+
+
+                //string userRoot = System.Environment.GetEnvironmentVariable("USERPROFILE");
+                //string downloadFolder = Path.Combine(userRoot, "Documents");
+                //Console.WriteLine(userRoot);
+                //Open excel report
+                //FileStream file2 = new FileStream(downloadFolder + "\\Export.xlsx", FileMode.Open, FileAccess.Read);
+                //XSSFWorkbook workbook2 = new XSSFWorkbook(file2);
+                //ISheet sheet2 = workbook2.GetSheet("Sheet1");
 
 
 
@@ -52,3 +68,6 @@ namespace ConsoleApp2
         }
     }
 }
+
+
+
